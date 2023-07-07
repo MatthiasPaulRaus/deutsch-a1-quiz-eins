18667ce35f06521cd1e482ebb470f5f3848db8ea
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-07-07 19:22:01 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Hallo! Ich heiße Noah. Und wie heißen Sie?",
+        answersList: [
+            {answer:"Ich heiße Clara Berger.", isCorrect:true},
+            {answer:"Du heißt Clara Berger.", isCorrect:false},
+            {answer:"Sie heißt Clara Berger.", isCorrect:false},
+        ]
+    },{
+        question:"Guten Tag! Ich bin Frau Müller. Und wie heißen Sie?",
+        answersList: [
+            {answer:"Ich bin Clara.", isCorrect:false},
+            {answer:"Ich heiße Clara Blanco.", isCorrect:true},
+            {answer:"Ich bin Blanco.", isCorrect:false},
+        ]
+    },{
+        question:"Ich komme aus Deutschland. Woher kommst du?",
+        answersList: [
+            {answer:"Du kommst aus Spanien.", isCorrect:false},
+            {answer:"Wir kommen aus Spanien.", isCorrect:false},
+            {answer:"Ich komme aus Italien.", isCorrect:true},
+        ]
+    },{
+        question:"Ich wohne in München. Und wo wohnt ihr?",
+        answersList: [
+            {answer:"Wir wohnen auch in München.", isCorrect:true},
+            {answer:"Du wohnst in München.", isCorrect:false},
+            {answer:"Sie wohnen in München.", isCorrect:false},
+        ]
+    },{
+        question:"Wir wohnen in Hamburg. Und wo wohnt ihr?",
+        answersList: [
+            {answer:"Sie wohnen in Berlin.", isCorrect:false},
+            {answer:"Wir wohnen in München.", isCorrect:true},
+            {answer:"Ihr wohnt in Hamburg.", isCorrect:false},
+        ]
+    },{
+        question:"Frau Bianco, wer ist das?",
+        answersList: [
+            {answer:"Hm, das ist meine Katze.", isCorrect:false},
+            {answer:"Das ist mein Auto.", isCorrect:false},
+            {answer:"Das ist meine Schwester Olga.", isCorrect:true},
+        ]
+    },{
+        question:"Wie viel kostet das Fahrrad?",
+        answersList: [
+            {answer:"Es kostet 259.- Euro.", isCorrect:true},
+            {answer:"Er kostet 195.- Euro.", isCorrect:false},
+            {answer:"Der kostet 350.- Euro.", isCorrect:false},
+        ]
+    },{
+        question:"Wie heißt das auf Deutsch?",
+        answersList: [
+            {answer:"Sie ist eine Uhr.", isCorrect:false},
+            {answer:"Das ist eine Uhr.", isCorrect:true},
+            {answer:"Das ist keine Uhr.", isCorrect:false},
+        ]
+    },{
+        question:"Wie viele Handys haben Sie?",
+        answersList: [
+            {answer:"Er hat drei Handys.", isCorrect:false},
+            {answer:"Ich habe moderne Handys.", isCorrect:false},
+            {answer:"Ich habe zwei Handys.", isCorrect:true},
+        ]
+    },{
+        question:"Wie finden Sie das Bild?",
+        answersList: [
+            {answer:"Wow, das gefällt mir sehr gut.", isCorrect:true},
+            {answer:"Ich finde sie nicht schön.", isCorrect:false},
+            {answer:"Ich finde es nicht praktisch.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Können Sie tanzen?",
+        answersList: [
+            {answer:"Ja, ich kann sehr gut tanzen.", isCorrect:true},
+            {answer:"Ja, sehr gern.", isCorrect:false},
+            {answer:"Nein, wir wollen nicht tanzen.", isCorrect:false},
+        ]
+    },{
+        question:"Was machen Sie in Ihrer Freizeit?",
+        answersList: [
+            {answer:"Sie haben keine Freizeit.", isCorrect:false},
+            {answer:"Ich gehe gern Joggen.", isCorrect:true},
+            {answer:"Ich habe viel Freizeit.", isCorrect:false},
+        ]
+    },{
+        question:"Wann ist das Wochenende?",
+        answersList: [
+            {answer:"Am Montag und Dienstag.", isCorrect:false},
+            {answer:"Am Donnerstag und Freitag.", isCorrect:false},
+            {answer:"Am Samstag und Sonntag.", isCorrect:true},
+        ]
+    },{
+        question:"Wann stehen Sie morgens auf?",
+        answersList: [
+            {answer:"Ich stehe um acht auf.", isCorrect:true},
+            {answer:"Wir stehen nicht früh auf.", isCorrect:false},
+            {answer:"Sie muss um acht aufstehen.", isCorrect:false},
+        ]
+    },{
+        question:"Wie spät ist es?",
+        answersList: [
+            {answer:"Es ist fünfzehn.", isCorrect:false},
+            {answer:"Es ist Viertel vor fünf.", isCorrect:true},
+            {answer:"Es ist fünfzehn vor halb sechs.", isCorrect:false},
+        ]
+    },{
+        question:"Wann frühstückst du?",
+        answersList: [
+            {answer:"Von 12 bis 13 Uhr.", isCorrect:false},
+            {answer:"Ich trinke einen Tee.", isCorrect:false},
+            {answer:"Hm, von acht bis halb neun.", isCorrect:true},
+        ]
+    },{
+        question:"Gibt es einen Bus zum Stadion?",
+        answersList: [
+            {answer:"Ja, es gibt einen direkten Bus.", isCorrect:true},
+            {answer:"Ja, es gibt ein Stadion.", isCorrect:false},
+            {answer:"Es gibt kein Stadion.", isCorrect:false},
+        ]
+    },{
+        question:"Können Sie mich morgen anrufen?",
+        answersList: [
+            {answer:"Ich telefoniere am Nachmittag.", isCorrect:false},
+            {answer:"Ja, ich rufe Sie am Nachmittag an.", isCorrect:true},
+            {answer:"Ich habe Ihr Handy nicht.", isCorrect:false},
+        ]
+    },{
+        question:"Was haben Sie gestern Abend gemacht?",
+        answersList: [
+            {answer:"Wir haben um vier eingekauft.", isCorrect:false},
+            {answer:"Ich muss die Wohnung aufräumen.", isCorrect:false},
+            {answer:"Ich habe mein Arbeitszimmer aufgeräumt.", isCorrect:true},
+        ]
+    },{
+        question:"Was habt ihr am Sonntag gemacht?",
+        answersList: [
+            {answer:"Wir haben eine Freundin besucht.", isCorrect:true},
+            {answer:"Ich habe am Freitag ein Konzert besucht.", isCorrect:false},
+            {answer:"Am Sonntag möchten wir Sport machen.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Wie haben Sie Ihren Geburtstag gefeiert?",
+        answersList: [
+            {answer:"Ich habe meine Freunde eingeladen.", isCorrect:true},
+            {answer:"Na, ich habe viele Sachen geschenkt.", isCorrect:false},
+            {answer:"Ich will eine Party machen.", isCorrect:false},
+        ]
+    },{
+        question:"Entschuldigung, wo ist denn hier der Bahnhof?",
+        answersList: [
+            {answer:"Dann gehen Sie weiter.", isCorrect:false},
+            {answer:"Sie können ihn nicht sehen.", isCorrect:true},
+            {answer:"Hier rechts und dann geradeaus.", isCorrect:false},
+        ]
+    },{
+        question:"Wann können wir uns treffen?",
+        answersList: [
+            {answer:"Sie treffen sich vor dem Museum.", isCorrect:false},
+            {answer:"Sie haben keine Zeit.", isCorrect:false},
+            {answer:"Um halb acht geht es.", isCorrect:true},
+        ]
+    },{
+        question:"Wo waren Sie denn im Urlaub?",
+        answersList: [
+            {answer:"Wir waren zwei Wochen in Italien.", isCorrect:true},
+            {answer:"Wir haben eine Pause gemacht.", isCorrect:false},
+            {answer:"Sie hatten keinen Urlaub.", isCorrect:false},
+        ]
+    },{
+        question:"He, wie findest du meine Hose?",
+        answersList: [
+            {answer:"Sie gefallen uns sehr gut.", isCorrect:false},
+            {answer:"Das gefällt mir nicht.", isCorrect:false},
+            {answer:"Die gefällt mir sehr gut.", isCorrect:true},
+        ]
+    },{
+        question:"Ich trage gern T-Shirts. Und Sie?",
+        answersList: [
+            {answer:"Ich trage am liebsten Pullover.", isCorrect:true},
+            {answer:"Er trägt nur Hemden.", isCorrect:false},
+            {answer:"Sie tragen auch gern T-Shirts.", isCorrect:false},
+        ]
+    },{
+        question:"Wann ist Ihr Geburtstag?",
+        answersList: [
+            {answer:"Wow, vierzig Jahre alt.", isCorrect:false},
+            {answer:"Am vierzehnten August.", isCorrect:true},
+            {answer:"Ich werde vierzig.", isCorrect:false},
+        ]
+    },{
+        question:"Was hat Ihnen am besten gefallen?",
+        answersList: [
+            {answer:"Wir besuchen lieber den Sportplatz.", isCorrect:false},
+            {answer:"Das Zentrum ist nicht weit entfernt.", isCorrect:false},
+            {answer:"Am besten fanden wir die Restaurants.", isCorrect:true},
+        ]
+    },{
+        question:"Warum sind Sie gestern nicht gekommen?",
+        answersList: [
+            {answer:"Ich hatte keine Lust auszugehen.", isCorrect:true},
+            {answer:"Ich wollte gern kommen.", isCorrect:false},
+            {answer:"Wir waren vorgestern im Kino.", isCorrect:false},
+        ]
+    },{
+        question:"Können wir den Termin noch verschieben?",
+        answersList: [
+            {answer:"Nein, wir haben keinen Termin.", isCorrect:false},
+            {answer:"Nein, Sie können später kommen.", isCorrect:false},
+            {answer:"Ja, auf nächste Woche.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
